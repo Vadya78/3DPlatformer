5fe7b68aad82954530f9ee41ad8a239993aca59d
Vadya78 <vadya78@gmail.com>
2017-10-10 16:42:37 +0300

    Exp. AI navigation

---
+++ Assets/Scripts/NavigationAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationAI : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] targets;
+    private int countOfTargets;
+    private int i = 0;
+
+	void Start ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        countOfTargets = targets.Length;
+        Debug.Log(countOfTargets);
+        navMeshAgent.SetDestination(targets[i].position);
+	}
+
+	void Update ()
+    {
+        if (!navMeshAgent.pathPending)
+        {
+            if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0)
+                {
+                    if(i < countOfTargets - 1)
+                    {
+                        i++;
+                        navMeshAgent.SetDestination(targets[i].position);
+                    }
+                    else
+                    {
+                        i = 0;
+                        navMeshAgent.SetDestination(targets[i].position);
+                    }
+                    Debug.Log("Stopped");
+                }
+            }
+        }
+
+
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+	}
+}
